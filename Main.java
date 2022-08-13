038bf33df29f0014febc68cec39a95a125ceb6fe
Pritu2022 <candlepritu21century@gmail.com>
2022-08-13 21:47:30 +0530

    dsa

---
+++ Main.java
@@ -0,0 +1,40 @@
+package DesimalToBinary;
+
+import java.util.*;
+
+public class Main {
+		
+	    public static void main(String[] args){
+	    	
+	    	
+	    	
+	    	
+//	    	System.out.println(5/2);
+//	    	
+//	    	System.out.println(5%2);
+	 	
+	    	System.out.println("Enter tc");
+	        Scanner input=new Scanner(System.in);
+	        int tc=input.nextInt();
+	        
+	        System.out.println(tc);
+	        
+	        for(int i=0; i<tc; i++){
+	            int num=input.nextInt();
+	           System.out.println(num);
+	           
+	           System.out.println(binaryByRecursion(1024));
+	        };
+	    }
+	    
+    public static int binaryByRecursion(int num){
+	        if(num==0){
+	            return 0;
+	        }	        
+	        else{
+	                return (num%2+10*(binaryByRecursion(num/2)));
+	        }
+	    }
+	
+
+}
---
+++ ReverseNumber.java
@@ -0,0 +1,25 @@
+package DesimalToBinary;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		int num=123;
+		int reverse=0;
+//		for(int i=0; i<3; i++) {   //for this first you have to find the length of digit by converting string 
+		                           //using loop.but in while loop this problem is not...
+//		  int x=num%10;
+//		   reverse=reverse*10+x;
+//		   num=num-x;
+//		   num=num/10;
+//			
+//		}
+		
+		while(num>0) {
+			reverse=reverse*10+num%10;
+			num=num-num%10;
+			num=num/10;
+		}
+      System.out.println(reverse);
+	}
+
+}
