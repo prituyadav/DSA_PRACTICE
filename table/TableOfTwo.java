bde588a6559b4e08dc0944cf690dccf71a4f529d
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2022-08-13 21:57:24 +0530

    Table of 2 is added

---
+++ table/TableOfTwo.java
@@ -0,0 +1,15 @@
+package table;
+
+public class TableOfTwo {
+
+	public static void main(String[] args) {
+		int x=0;
+		int j=1;
+		for(int i=0; i<10; i++) {
+			x+=2;
+			System.out.println("2*"+j+" = "+x);
+			j++;
+		}
+	}
+
+}
