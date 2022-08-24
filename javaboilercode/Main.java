5f05ef724dec6a1b4bcfdf7f7389a053dea14a35
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2022-08-24 15:43:01 +0530

    STS

---
+++ javaboilercode/Main.java
@@ -0,0 +1,38 @@
+package javaboilercode;
+
+import java.util.*;
+import java.io.*;
+class Main{
+    static class FastReader
+  {
+    BufferedReader br;
+    StringTokenizer st;
+    
+    public FastReader()
+    {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+    String next()
+    {
+      while (st == null || !st.hasMoreElements()){
+        try { st = new StringTokenizer(br.readLine()); }
+        catch (IOException e) { e.printStackTrace(); }
+      }
+      return st.nextToken();
+    }
+    int nextInt() { return Integer.parseInt(next()); }
+    long nextLong() { return Long.parseLong(next()); }
+    double nextDouble(){ return Double.parseDouble(next()); }
+    String nextLine(){
+      String str = "";
+      try { str = br.readLine(); }
+      catch (IOException e) { e.printStackTrace(); }
+      return str;
+    }
+  }
+  public static void main(String[] args){
+      FastReader fs=new FastReader();
+      int tc=fs.nextInt();
+        
+  }
+}
