e76063580bd7af7a496e9fc88b87447e07952585
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-05-08 06:46:11 +0530

    Update PalindromeNumber.java

--- LeetCode/PalindromeNumber.java
+++ LeetCode/PalindromeNumber.java
@@ -1,1 +1,13 @@
-
+class Solution {
+    public boolean isPalindrome(int x) {
+         if (x < 0) return false;
+        int ans = 0;
+        int num = x;
+        while ( num > 0) {
+            ans = ans * 10 + num % 10;
+            num = num / 10;
+        }
+        if (ans != x) return false;
+        return true;
+    }
+}
