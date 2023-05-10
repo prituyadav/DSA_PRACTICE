b639a58ae74936ec45739d5056a01267c455d04c
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-05-10 07:44:18 +0530

    48. Rotate Image

---
+++ LeetCode/RotateImage.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix[0].length;
+        Stack<Integer> st=new Stack<>();
+        for(int i=0; i<n; i++){
+            for(int j=n-1; j>=0; j--){
+                st.push(matrix[i][j]);
+            }
+        }
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int x=st.pop();
+                matrix[j][i]=x;
+            }
+        }
+        
+    }
+}
