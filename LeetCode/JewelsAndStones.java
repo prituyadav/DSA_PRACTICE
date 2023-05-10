b75a98b5fdb66468e095f1275d8eb12e94ad299c
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-05-10 07:42:04 +0530

    771. Jewels and Stones

--- LeetCode/JewelsAndStones.java
+++ LeetCode/JewelsAndStones.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        if(J.isEmpty()) return 0;
+		int cnt = 0;
+		for(int i = 0 ; i < S.length() ; i++) {
+			if(J.indexOf(S.charAt(i)) != -1) cnt++;
+		}
+		return cnt;
+    }
+}
