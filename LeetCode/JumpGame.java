79fc2af59bfb8816439a993f79b2e2ff9cecaaba
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-05-10 07:53:18 +0530

    55. Jump Game

---
+++ LeetCode/JumpGame.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int lastTrue = n-1;
+        for(int i = n-2;i>=0;i--){
+            int jumpSize = nums[i];
+            if(i+jumpSize >= n-1 || i+jumpSize>=lastTrue){ //can you jump from here and reach the end or atleast reach the point that can help reach the end?
+                lastTrue = i;
+            }
+        }
+        return lastTrue == 0;
+    }
+}
