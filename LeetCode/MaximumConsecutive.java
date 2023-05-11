ca9b2b742a185fb8b5ade51b4e8e4bdc602572d8
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-05-11 16:47:15 +0530

    Update MaximumConsecutive.java

--- LeetCode/MaximumConsecutive.java
+++ LeetCode/MaximumConsecutive.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int maxConsecutive(int bottom, int top, int[] special) {
+        int maxDiff = Integer.MIN_VALUE;
+        Arrays.sort(special);
+        maxDiff = Math.max(maxDiff, special[0] - bottom);
+        maxDiff = Math.max(maxDiff, top - special[special.length - 1]);
+        
+        for (int i = 0; i < special.length - 1; i++) {
+            int floor1 = special[i];
+            int floor2 = special[i + 1];
+            
+            int diff = floor2 - floor1 - 1;
+            maxDiff = Math.max(maxDiff, diff);
+        }
+        
+        return maxDiff;
+    }
+}
