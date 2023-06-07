7692945d33b6946b824edbd303f9707da991ab7d
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-06-07 14:31:25 +0530

    162. Find Peak Element

---
+++ LeetCode/FindPeakElement.java
@@ -0,0 +1,33 @@
+class Solution {
+   public int findPeakElement(int[] num) {    
+    return helper(num,0,num.length-1);
+}
+
+public int helper(int[] num,int start,int end){
+    if(start == end){
+        return start;
+    }else if(start+1 == end){
+        if(num[start] > num[end]) return start;
+        return end;
+    }else{
+        
+        int m = (start+end)/2;
+        
+        if(num[m] > num[m-1] && num[m] > num[m+1]){
+
+            return m;
+
+        }else if(num[m-1] > num[m] && num[m] > num[m+1]){
+
+            return helper(num,start,m-1);
+
+        }else{
+
+            return helper(num,m+1,end);
+
+        }
+        
+    }
+}
+
+}
