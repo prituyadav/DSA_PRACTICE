2dceda7fac384c0b35b7f4d3ae77c83d2f17c7ce
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-06-08 22:05:10 +0530

    1768. Merge Strings Alternately

--- MergeStringsAlternately.java
+++ MergeStringsAlternately.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < Integer.max(word1.length(), word2.length()); i++) {
+            if(i < word1.length()) {
+                sb.append(word1.charAt(i));
+            }
+            if(i < word2.length()) {
+                sb.append(word2.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
