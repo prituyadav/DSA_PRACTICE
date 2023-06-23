459a17d7d2f72c85afbd83c30b3595a0b46ca216
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-06-23 23:30:06 +0530

    Create main.java

---
+++ HackerRank Challenge Of 120 problems/6.Compare the Triplets/main.java
@@ -0,0 +1,24 @@
+ public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        int alice=0;
+        int bob=0;
+        
+        List<Integer> ans=new ArrayList<>();
+        
+        for(int i=0; i<a.size(); i++){
+            if(a.get(i)>b.get(i)){
+                alice++;
+            }
+            else if(b.get(i)>a.get(i)){
+                bob++;
+            }
+            else{
+                continue;
+            }
+        }      
+          
+        ans.add(alice);
+        ans.add(bob);
+        
+        return ans;
+        
+    }
