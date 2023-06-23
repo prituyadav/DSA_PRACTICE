b496df8383409567ff7aaf3e71101c72cf8f3f70
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-06-23 22:29:26 +0530

    Update main.java

--- HackerRank Challenge Of 120 problems/Staircase/main.java
+++ HackerRank Challenge Of 120 problems/Staircase/main.java
@@ -1,1 +1,15 @@
+public static void main(int n) {
+     for(int i=1; i<n+1; i++){
+            for(int j=0; j<n; j++){
+                if(j<(n-i)){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("#");
+                }
+            }
+             System.out.println();
+        }
+        
 
+    }
