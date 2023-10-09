83a8580c1d34ee00d0d2d7248ccc6e48ed1d6a91
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-10-09 13:07:28 +0000

    count pairs

--- LeetCode/Countpairs.java
+++ LeetCode/Countpairs.java
@@ -1,6 +1,20 @@
+import java.util.List;
+
 public class Countpairs {
     public static void main(String[] args){
         System.out.println(1222);
+        //call your tc here.
+       System.out.println(solution(null, 0));
+    }
+
+    public static int solution(List<Integer> nums, int target){
+        int cnt = 0;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if(nums.get(i) + nums.get(j) < target)  cnt++;
+            }
+        }
+        return cnt;
     }
     
 }
