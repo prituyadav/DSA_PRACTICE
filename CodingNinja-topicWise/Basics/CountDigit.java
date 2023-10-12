df140c6804a14e5af863d29c72bcc2cf6f6cef72
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-10-12 05:17:19 +0000

    basic

---
+++ CodingNinja-topicWise/Basics/CountDigit.java
@@ -0,0 +1,8 @@
+public class CountDigit{
+    public static void main(String[] args) {
+        int a=123456;
+        String count = Integer.toString(a);
+
+        System.out.println(count.length()); 
+    }
+}
