6f9c6eebc78aa14428240f9b6eed2fe7a31b53ac
Pritu Yadav <101568121+Pritu2022@users.noreply.github.com>
2023-10-23 03:52:12 +0000

    is anagram

---
+++ LeetCode/IsAnagram.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class IsAnagram{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str= sc.next();
+        String ana=sc.next();
+
+        System.out.println(isAnagram(str, ana));
+    }
+
+     public static boolean isAnagram(String s, String t) {
+         char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
+    }
+}
